f90fe0f683c712bd978729e5a05f3189f4c994ab
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-05 17:03:09 -0700

    update 5:02pm
    Set items to  local storage as strings.

--- book.js
+++ book.js
@@ -1,8 +1,47 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-
-function saveBookmark(){
+//save Bookmark
+function saveBookmark(e){
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
     
     
+    var bookmark = {
+        name: siteName,
+        url:siteUrl
+    }
+    
+    
+//    //local storage Test
+//    localStorage.setItem('test', 'Hello World');
+//    console.log(localStorage.getItem('test'));
+//    localStorage.removeItem('test');
+//    console.log(localStorage.getItem('test'));
+    
+    //Test if bookmarks is null
+    
+    
+    if(localStorage.getItem('bookmarks') === null){
+        var bookmarks = [];
+        
+        bookmarks.push(bookmark);
+        
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }else{
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    //Prevent from submitting
     e.preventDefault();
 }
