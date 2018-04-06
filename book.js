a111cf4f2aedb0287906292ea55a20f35e82898b
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-05 20:23:00 -0700

    update 8:22pm

--- book.js
+++ book.js
@@ -45,3 +45,33 @@ function saveBookmark(e){
     //Prevent from submitting
     e.preventDefault();
 }
+
+
+//delete bookmark
+function deleteBookmark(url){
+    console.log(url);
+}
+
+
+//Fetch bookmarks
+function fetchBookmarks(){
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+     
+    
+//Get output id 
+var bookmarksResults = document.getElementById('bookmarksResults');
+    
+//Build output
+    
+bookmarksResults.innerHTML = '';
+ for(var i = 0; i < bookmarks.length; i++){
+     var name= bookmarks[i].name;
+     var url = bookmarks[i].url;
+     
+     bookmarksResults.innerHTML += '<div class="well">'+
+                                   '<h3>' +name+ 
+                                   ' <a class="btn btn-info" target="_blank" href="' +url+'">Visit</a> ' +
+                                    ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                   '</h3>'+ '</div>';
+ }
+}
