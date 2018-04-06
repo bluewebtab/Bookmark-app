715773de41722b51ca400085db4ef5c34b11634a
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-05 20:59:11 -0700

    update 8:59pm

--- book.js
+++ book.js
@@ -6,7 +6,10 @@ function saveBookmark(e){
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
     
-    
+    if(!validateForm(siteName, siteUrl)){
+       return false;
+       }
+   
     var bookmark = {
         name: siteName,
         url:siteUrl
@@ -37,9 +40,12 @@ function saveBookmark(e){
     }
     
     
+    //clear form
+    document.getElementById('myForm').reset();
     
     
-    
+    //Be fetch bookmarks
+    fetchBookmarks();
     
     
     //Prevent from submitting
@@ -50,6 +56,21 @@ function saveBookmark(e){
 //delete bookmark
 function deleteBookmark(url){
     console.log(url);
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    
+    //loop through bookmarks
+    for(var i = 0; i < bookmarks.length; i++){
+        if(bookmarks[i].url == url){
+            bookmarks.splice(i, 1);
+            
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    
+    
+    //Re-fetch bookmarks
+    fetchBookmarks();
+    
 }
 
 
@@ -74,4 +95,20 @@ bookmarksResults.innerHTML = '';
                                     ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
                                    '</h3>'+ '</div>';
  }
+}
+
+function validateForm(siteName, siteUrl){
+     if(!siteName || !siteUrl){
+        alert('Please fill in the form')
+        return false;
+       }
+    
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    
+    if(!siteUrl.match(regex)){
+        alert('Please use a valid URL');
+        return false;
+    }
+    return true;
 }
